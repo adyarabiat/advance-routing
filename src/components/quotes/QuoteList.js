ccdddec2f724becfc59ce4d99f1c4704e5934145
adyarabiat <ady.arabiat@gmail.com>
2021-06-08 18:11:17 +0300

    Adding  Query Parameters to get things from the URL

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,49 @@
 import { Fragment } from "react";
-
+import { useHistory, useLocation } from "react-router-dom";
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+// we add it to sort the quotes
+const sortQuotes = (quotes, ascending) => {
+   return quotes.sort((quoteA, quoteB) => {
+      if (ascending) {
+         return quoteA.id > quoteB.id ? 1 : -1;
+      } else {
+         return quoteA.id < quoteB.id ? 1 : -1;
+      }
+   });
+};
+
 const QuoteList = (props) => {
+   const history = useHistory();
+   const location = useLocation();
+   console.log(location.search);
+
+   // this will give me a better constructure object value instead of ?sort=asc
+   const queryParams = new URLSearchParams(location.search);
+
+   // we use get to get the key which from it ?sort=asc so it is sort
+   const SortingAscending = queryParams.get("sort");
+
+   // checking
+   const isSortingAscending = SortingAscending === "asc";
+
+   // now we are calling the function to sort
+   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+   const changeSortingHandler = () => {
+      history.push(`/quotes?sort=${isSortingAscending ? "desc" : "asc"}`);
+   };
+
    return (
       <Fragment>
+         <div className={classes.sorting}>
+            <button onClick={changeSortingHandler}>
+               Sort {isSortingAscending ? "Descending" : "Ascending"}
+            </button>
+         </div>
          <ul className={classes.list}>
-            {props.quotes.map((quote) => (
+            {sortedQuotes.map((quote) => (
                <QuoteItem
                   key={quote.id}
                   id={quote.id}
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -19,9 +19,22 @@ const QuoteDetails = () => {
       return <p> No Quote found</p>;
    }
 
+   // we add this route here around the link just to show it when we are on the exact path without the comment so it will disappear when we finish
    return (
       <React.Fragment>
          <HighlightedQuote text={quote.text} author={quote.author} />
+
+         <Route path={`/quotes/${param.quote}`} exact>
+            <div className="centered">
+               <Link
+                  className="btn--flat"
+                  to={`/quotes/${param.quote}/comments`}
+               >
+                  Add Comment
+               </Link>
+            </div>
+         </Route>
+
          <Route path={`/quotes/${param.quote}/comments`}>
             <Comments />
          </Route>
