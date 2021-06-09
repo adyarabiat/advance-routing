8df5882b6fea0be43abb9e33d0cb8cb85b2a4584
adyarabiat <ady.arabiat@gmail.com>
2021-06-09 10:11:38 +0300

    Adding lazy loading

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
+import React, { Suspense } from "react";
+
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import Quotes from "./pages/Quotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetails from "./pages/QuoteDetails";
+// import Quotes from "./pages/Quotes";
+// import NewQuote from "./pages/NewQuote";
+// import QuoteDetails from "./pages/QuoteDetails";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const Quotes = React.lazy(() => import("./pages/Quotes"));
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetails = React.lazy(() => import("./pages/QuoteDetails"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+// lazy loading :
+
+// import NewQuote from "./pages/NewQuote";
+// change it to
+// const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+// then we import { Suspense } from 'react
+// then we wrrape the application with the Suspense and we pass  fallback to do something whilst it is loading
 
 function App() {
    return (
       <Layout>
-         <Switch>
-            <Route path="/" exact>
-               <Redirect to="/quotes" />
-            </Route>
-            <Route path="/quotes" exact>
-               <Quotes />
-            </Route>
-            <Route path="/quotes/:quote">
-               <QuoteDetails />
-            </Route>
-            <Route path="/new-quote">
-               <NewQuote />
-            </Route>
-            <Route path="*">
-               <NotFound />
-            </Route>
-         </Switch>
+         <Suspense
+            fallback={
+               <div className="centered">
+                  <LoadingSpinner />
+               </div>
+            }
+         >
+            <Switch>
+               <Route path="/" exact>
+                  <Redirect to="/quotes" />
+               </Route>
+               <Route path="/quotes" exact>
+                  <Quotes />
+               </Route>
+               <Route path="/quotes/:quote">
+                  <QuoteDetails />
+               </Route>
+               <Route path="/new-quote">
+                  <NewQuote />
+               </Route>
+               <Route path="*">
+                  <NotFound />
+               </Route>
+            </Switch>
+         </Suspense>
       </Layout>
    );
 }
